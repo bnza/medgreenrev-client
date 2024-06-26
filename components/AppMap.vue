01f9324e099b4e9b75fbff5e641718226bd55836
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2024-06-26 17:40:39 +0200

    feat(core) add vue3-openlayers

--- components/AppMap.vue
+++ components/AppMap.vue
@@ -1,3 +1,27 @@
+<script setup lang="ts">
+import { Map, Layers, Sources } from 'vue3-openlayers'
+const center = ref([40, 40])
+const projection = ref('EPSG:4326')
+const zoom = ref(8)
+const rotation = ref(0)
+</script>
+
 <template>
-  <p>map</p>
+  <Map.OlMap
+    :loadTilesWhileAnimating="true"
+    :loadTilesWhileInteracting="true"
+    style="height: 100%; width: 100%"
+  >
+    <Map.OlView
+      ref="view"
+      :center.sync="center"
+      :rotation="rotation"
+      :zoom.sync="zoom"
+      :projection="projection"
+    />
+
+    <Layers.OlTileLayer>
+      <Sources.OlSourceOsm />
+    </Layers.OlTileLayer>
+  </Map.OlMap>
 </template>
