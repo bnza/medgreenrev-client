4f472d73c03257bb087cd101cb957de68be1de63
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2024-10-10 14:25:42 +0200

    refactor(core): improve data collection table fetching

--- app/components/LoadingComponent.vue
+++ app/components/LoadingComponent.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container class="h-100 w-100 align-center justify-center">
-    <v-row>
-      <v-col>
+  <v-container class="d-flex h-100 w-100">
+    <v-row align="center" justify="center">
+      <v-col cols="3" class="d-flex justify-center align-center">
         <v-progress-circular indeterminate :size="117" :width="12" />
       </v-col>
     </v-row>
--- app/components/data/collection/DataCollectionTable.vue
+++ app/components/data/collection/DataCollectionTable.vue
@@ -19,23 +19,29 @@ if (props.parent) {
   parent.value = props.parent
 }
 
-const { items, paginationOptions, totalItems, status } = await fetchCollection()
+// @ts-ignore
+let results: Ref<Partial<Awaited<ReturnType<typeof fetchCollection>>>> = ref({
+  items: ref([]),
+  status: ref('pending'),
+})
+
+fetchCollection().then((_results) => (results.value = _results))
 </script>
 
 <template>
   <v-data-table-server
-    :loading="status === 'pending'"
+    :loading="results.status === 'pending'"
     fixed-header
     fixed-footer
     height="calc(100vh - 300px)"
     :headers
-    :items
-    :items-length="totalItems"
-    :items-per-page="paginationOptions.itemsPerPage"
+    :items="results.items"
+    :items-length="results.totalItems || 0"
+    :items-per-page="results.paginationOptions?.itemsPerPage"
     :items-per-page-options
-    :page="paginationOptions.page"
-    :sort-by="paginationOptions.sortBy"
-    @update:options="paginationOptions = $event"
+    :page="results.paginationOptions?.page || 1"
+    :sort-by="results.paginationOptions?.sortBy"
+    @update:options="results.paginationOptions = $event"
   >
     <!-- https://mokkapps.de/vue-tips/expose-slots-from-a-child-component-->
     <template v-for="(_, name) in $slots" #[name]="slotProps">
@@ -46,6 +52,8 @@ const { items, paginationOptions, totalItems, status } = await fetchCollection()
       />
     </template>
   </v-data-table-server>
+
+  <!--  <loading-component v-else />-->
 </template>
 
 <style scoped></style>
